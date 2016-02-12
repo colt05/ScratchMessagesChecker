44bd70a5ec0ce9d57a05eabadf23a7e2c17dc988
colt05 <824ba30a@opayq.com>
2016-02-12 16:09:02 -0500

    it's not freezing, it's waiting for a response from the scratch api

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,11 +24,12 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(80, 185)
+        Me.Button1.Location = New System.Drawing.Point(77, 172)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(109, 65)
         Me.Button1.TabIndex = 0
@@ -43,11 +44,21 @@ Partial Class Form1
         Me.TextBox1.TabIndex = 1
         Me.TextBox1.Text = "Username"
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(71, 240)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(128, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "(may take a few seconds)"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
@@ -59,4 +70,5 @@ Partial Class Form1
 
     Friend WithEvents Button1 As Button
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
 End Class
